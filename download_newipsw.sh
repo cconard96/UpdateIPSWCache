d6a5052c01168f6b9604ae149bdee03d0deaf9dd
Curtis Conard <cconard96@gmail.com>
2019-10-28 16:35:32 -0400

    Update download_newipsw.sh

--- download_newipsw.sh
+++ download_newipsw.sh
@@ -1,7 +1,7 @@
 # Some global variables
 CONFIGURATOR_FIRMWARE_DIR="~/Library/Group Containers/K36BKF7T3D.group.com.apple.configurator/Library/Caches/Firmware/"
-echo "$CONFIGURATOR_FIRMWARE_DIR"
-#exit 0
+WORKING_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+IDENTIFIER_FILE=$WORKING_DIR/identifiers.txt
 
 echo "Starting IPSW firmware check $(date)"
 
@@ -15,9 +15,9 @@ while read identifier; do
    # See if this latest firmware is in the Configurator cache
    if [ ! -f "$CONFIGURATOR_FIRMWARE_DIR/$LATEST_FIRMWARE_NAME" ]; then
       echo "Need $LATEST_FIRMWARE_NAME. Downloading..."
-      # Ask IPSW what the Apple server download URL is
+      # Ask IPSW.me what the Apple server download URL is
       LATEST_FIRMWARE_URL="$(wget -qO- http://api.ipsw.me/v2.1/$identifier/latest/url)"
-      # Download latest firmware to the Confirguator cache
+      # Download latest firmware to the Configurator cache
       wget $LATEST_FIRMWARE_URL -P "$CONFIGURATOR_FIRMWARE_DIR"
    else
       echo "Already have latest firmware for $identifier"
@@ -25,6 +25,6 @@ while read identifier; do
 
    # Sleep for a second as a courtesy to IPSW.me
    sleep 1
-done < ~/used_appledevices.txt
+done < $IDENTIFIER_FILE
 
 echo "Finished IPSW firmware check $(date)"
